8d208fcd0f0a1c49690020fa39b0b6839324977f
Rae Carter <92331343+heimdalalr@users.noreply.github.com>
2021-12-24 10:54:08 -0500

    sales report comments added

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,25 @@
 """Generate sales report showing total melons each salesperson sold."""
 
 
-salespeople = []
-melons_sold = []
+salespeople = [] # creates an empty list for sales people
+melons_sold = [] #creates and empty list for melons sold
 
-f = open('sales-report.txt')
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+f = open('sales-report.txt') #variable f opens the sales-report document
+for line in f: #loops the file opened with variable f
+    line = line.rstrip() #returns a copy of the string by removing the trailing characters specified as argument
+    entries = line.split('|') #split method divides a string into a list
 
-    salesperson = entries[0]
-    melons = int(entries[2])
+    salesperson = entries[0] #sales people are in the 0 index of the list
+    melons = int(entries[2]) #amount of melons sold are in the third index of the list
 
     if salesperson in salespeople:
-        position = salespeople.index(salesperson)
+        position = salespeople.index(salesperson) #index method returns the value of the first occurence of salesperson in salespeople
 
-        melons_sold[position] += melons
+        melons_sold[position] += melons #
     else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+        salespeople.append(salesperson) #otherwise adds salesperson to end of list salespeople
+        melons_sold.append(melons) #otherwise adds melons sold to end of list melons
 
 
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+for i in range(len(salespeople)): #i declares the reproducibility of the legnth of sales people
+    print(f'{salespeople[i]} sold {melons_sold[i]} melons') #prints from the list of salespeople to the amount of melons sold
